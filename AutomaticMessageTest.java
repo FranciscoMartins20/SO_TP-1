ef7abbb90405d09b0a0786a239554aecad38d7a4
Francisco Martins <jfmartins200@gmail.com>
2024-01-26 19:03:08 +0000

    Test

---
+++ AutomaticMessageTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+public class AutomaticMessageTest {
+    @Test
+    public void testGenerateAutomaticMessage() {
+
+    }
+
+    @Test
+    public void testGetActiveThreadCount() {
+
+    }
+
+    @Test
+    public void testRun() {
+
+    }
+
+    @Test
+    public void testStopThread() {
+
+    }
+}
